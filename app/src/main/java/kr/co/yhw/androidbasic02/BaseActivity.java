93d33e7bf83e04cab43a6dff07fc0f311231d174
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-21 13:09:32 +0900

    사용자 정보화면=>BaseActivity로 상속

--- app/src/main/java/kr/co/yhw/androidbasic02/BaseActivity.java
+++ app/src/main/java/kr/co/yhw/androidbasic02/BaseActivity.java
@@ -1,8 +1,12 @@
 package kr.co.yhw.androidbasic02;
 
+import android.content.Context;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
 
     public abstract void setupEvents();
     public abstract void setupValues();
--- app/src/main/java/kr/co/yhw/androidbasic02/MainActivity.java
+++ app/src/main/java/kr/co/yhw/androidbasic02/MainActivity.java
@@ -3,7 +3,9 @@ package kr.co.yhw.androidbasic02;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import kr.co.yhw.androidbasic02.databinding.ActivityMainBinding;
 
@@ -19,7 +21,17 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String inputName = binding.nameEdt.getText().toString();
+
+                Intent intent = new Intent(mContext,UserInfoActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
     @Override
